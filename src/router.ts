da2eaa486b3a934579330ca2b3c51eafb7c9ec06
hloza <hloza@ucb.edu.bo>
2025-03-27 15:13:23 -0400

    implementacion de rutas

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,10 @@
 import { Router } from 'express';
 import authRoutes from './modules/auth/routes';
 import userRoutes from './modules/user/routes';
-
+import skillRoutes from './modules/skill/routes';
+import projectRoutes from './modules/project/routes';
+import experienceRoutes from './modules/experiencia/routes';
+import educationRoutes from './modules/education/routes';
 
 
 const router = Router();
@@ -11,6 +14,11 @@ const router = Router();
 // Definir las rutas de los módulos
 router.use('/user', authRoutes);
 router.use('/user', userRoutes);
+router.use('/skill', skillRoutes);
+router.use('/project', projectRoutes);
+router.use('/experience', experienceRoutes);
+router.use('/education', educationRoutes);
+
 // Importar las rutas de los módulos
 
 export default router;
